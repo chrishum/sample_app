6cdb5bb4a18d5539bb25b0fb4621d26ef6dec282
Chris Humphreys <humphreys.chris@gmail.com>
2011-04-22 15:36:35 -0700

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,16 @@ module ApplicationHelper
   # Return a title on a per-page basis
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
-	if @title.nil?
-	  base_title
-	else
-	  "#{base_title} | #{@title}"
-	end
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
+  def logo
+    logo_file = "logo.png"
+    logo_alt = "sample App"
+    image_tag(logo_file, :alt => logo_alt, :class => 'round')
   end
 end
